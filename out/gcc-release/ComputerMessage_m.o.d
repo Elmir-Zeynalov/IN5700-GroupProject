40c3e0cfa64dc89134514e9c5239942f5a2a4277
Elmir-Zeynalov <elmirzeynalov1@gmail.com>
2023-10-31 17:08:14 +0100

    adding all files x.x

---
+++ out/gcc-release/ComputerMessage_m.o.d
@@ -0,0 +1,5 @@
+out/gcc-release//ComputerMessage_m.o: ComputerMessage_m.cc \
+ ComputerMessage_m.h \
+ C:/Users/elmir/Documents/Omnetto/omnetpp-5.7/include/omnetpp.h
+ComputerMessage_m.h:
+C:/Users/elmir/Documents/Omnetto/omnetpp-5.7/include/omnetpp.h:
---
+++ out/gcc-release/oblig.o.d
@@ -0,0 +1,5 @@
+out/gcc-release//oblig.o: oblig.cc \
+ C:/Users/elmir/Documents/Omnetto/omnetpp-5.7/include/omnetpp.h \
+ ComputerMessage_m.h
+C:/Users/elmir/Documents/Omnetto/omnetpp-5.7/include/omnetpp.h:
+ComputerMessage_m.h:
